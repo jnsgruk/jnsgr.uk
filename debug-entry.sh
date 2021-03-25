5324c692a38e168bdd3435bc439bf7a9e415d0df
Jon Seager <jon@sgrs.uk>
2021-03-25 08:19:06 +0000

    Updates to the debug image

--- debug-entry.sh
+++ debug-entry.sh
@@ -1,5 +1,3 @@
 #!/bin/ash
-export REDIRECT_MAP_URL="https://gist.githubusercontent.com/jnsgruk/b590f114af1b041eeeab3e7f6e9851b7/raw/cacd49f5245fce3ad418e76ce469b7ebf91a688a/routes"
-export WEBROOT="/srv"
-
-/usr/bin/gosherve > /tmp/output.log 2>&1
+mkdir -p /var/log/gosherve
+/usr/bin/gosherve > "/var/log/gosherve/gosherve-$(date +"%s").log" 2>&1
