cac443bfea9962bbd36470b0aa963e739f73d5df
Jon Seager <jon@sgrs.uk>
2024-01-11 21:48:58 +0000

    ci: deploy to Fly using nix built container

--- flake.nix
+++ flake.nix
@@ -20,36 +20,50 @@
       });
     in
     {
-      packages = forAllSystems (system:
-        let
-          inherit (pkgsForSystem system) buildGoModule hugo cacert;
-          inherit (self) lastModifiedDate;
-          version = self.rev or self.dirtyRev or "dirty";
-        in
-        rec {
-          default = jnsgruk;
-          jnsgruk = buildGoModule {
-            inherit version;
-            pname = "jnsgruk";
-            src = self;
-            vendorHash = "sha256-bHGM+4aL2rjddEGXd4RGUFLK7/gTc2fMGa4KqLou0lk=";
-            buildInputs = [ cacert ];
-            nativeBuildInputs = [ hugo ];
+      packages = forAllSystems
+        (system:
+          let
+            inherit (pkgsForSystem system) buildGoModule buildEnv dockerTools hugo lib cacert;
+            inherit (self) lastModifiedDate;
+            version = self.rev or "dirty";
+          in
+          rec {
+            default = jnsgruk;
+            jnsgruk = buildGoModule {
+              inherit version;
+              pname = "jnsgruk";
+              src = lib.cleanSource ./.;
+              vendorHash = "sha256-bHGM+4aL2rjddEGXd4RGUFLK7/gTc2fMGa4KqLou0lk=";
+              buildInputs = [ cacert ];
+              nativeBuildInputs = [ hugo ];
 
-            preBuild = ''
-              go generate ./...
-            '';
+              preBuild = ''
+                go generate ./...
+              '';
 
-            ldflags = [
-              "-X main.commit=${version}"
-              "-X main.date=${lastModifiedDate}"
-            ];
+              ldflags = [
+                "-X main.commit=${version}"
+                "-X main.date=${lastModifiedDate}"
+              ];
 
-            postInstall = ''
-              mv $out/bin/jnsgr.uk $out/bin/jnsgruk
-            '';
-          };
-        });
+              postInstall = ''
+                mv $out/bin/jnsgr.uk $out/bin/jnsgruk
+              '';
+            };
+
+            container = dockerTools.buildImage {
+              name = "jnsgruk/jnsgr.uk";
+              tag = version;
+              created = "now";
+              copyToRoot = buildEnv {
+                name = "image-root";
+                paths = [ jnsgruk cacert ];
+                pathsToLink = [ "/bin" "/etc/ssl/certs" ];
+              };
+              config.Entrypoint = [ "${jnsgruk}/bin/jnsgruk" ];
+              config.User = "1000:1000";
+            };
+          });
 
       devShells = forAllSystems (system:
         let
@@ -65,6 +79,7 @@
               gofumpt
               gopls
               hugo
+              flyctl
               zsh
             ];
             shellHook = "exec zsh";
